45c50d4dec902b17ff8e602dd0e11695499d2c52
qingyunleong <qing.yun.leong@gmail.com>
2022-06-06 14:56:26 +0800

    a

--- php/course.php
+++ php/course.php
@@ -5,6 +5,34 @@ if (!isset($_SESSION['sessionid'])) {
     echo "<script>alert('Session not available. Please login');</script>";
     echo "<script> window.location.replace('login.php')</script>";
 }
+
+$results_per_page = 10;
+if (isset($_GET['pageno'])) {
+    $pageno = (int)$_GET['pageno'];
+    $page_first_result = ($pageno - 1) * $results_per_page;
+} else {
+    $pageno = 1;
+    $page_first_result = 0;
+}
+
+include_once("dbconnect.php");
+
+$sqlsubjects = "SELECT * FROM tbl_subjects";
+$stmt = $conn->prepare($sqlsubjects);
+$stmt->execute();
+$number_of_result = $stmt->rowCount();
+$number_of_page = ceil($number_of_result / $results_per_page);
+$sqlsubjects = $sqlsubjects . " LIMIT $page_first_result , $results_per_page";
+$stmt = $conn->prepare($sqlsubjects);
+$stmt->execute();
+$result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+$rows = $stmt->fetchAll();
+
+
+function truncate($string, $length, $dots = "...")
+{
+    return (strlen($string) > $length) ? substr($string, 0, $length - strlen($dots)) . $dots : $string;
+}
 ?>
 
 <!DOCTYPE html>
@@ -48,7 +76,66 @@ if (!isset($_SESSION['sessionid'])) {
     </div>
     <br>
 
-    <footer class="w3-footer w3-center w3-bottom" style="background-color: #133764 ; color:white">
+    <div style="max-width:1100px; margin:0 auto; background-color: #133764; color:white">
+        <div class="w3-container">
+            <h3>Courses</h3>
+        </div>
+    </div>
+    <br>
+
+    <div style="max-width:1000px;margin:0 auto;">
+
+        <div class="w3-grid-template">
+            <?php
+            $i = 0;
+            foreach ($rows as $subjects) {
+                $i++;
+                $subjectid = $subjects['subject_id'];
+                $subjectname = truncate($subjects['subject_name'], 15);
+                $subjectdesc = $subjects['subject_description'];
+                $subjectprice = number_format((float)$subjects['subject_price'], 2, '.', '');
+                $tutorid = $subjects['tutor_id'];
+                $subjectsession = $subjects['subject_sessions'];
+                $subjectrating = $subjects['subject_rating'];
+
+                echo "<div class='w3-card w3-round' style='margin:4px'>";
+                echo "<br><a href='coursedetails.php?subjectid=$subjectid' style='text-decoration: none;'> <center><img class='w3-image w3-circle' src='../assets/courses/$subjectid.png'" .
+                    " onerror=this.onerror=null;this.src='../res/background.png'"
+                    . " style='width:70%;height:170px'></a>";
+                echo "<div class='w3-container'><p><h5><b>$subjectname</b></h5>
+                      <i class='fa fa-star fa-fw'></i>$subjectrating<p>
+                      <div class='w3-container w3-leftbar w3-light-grey'>RM $subjectprice</div>
+                      <div class='w3-container w3-leftbar w3-light-grey'>$subjectsession classes</div>
+                      </p></div>
+                </div>";
+            }
+            ?>
+        </div>
+
+        <br>
+        <?php
+        $num = 1;
+        if ($pageno == 1) {
+            $num = 1;
+        } else if ($pageno == 2) {
+            $num = ($num) + 10;
+        } else {
+            $num = $pageno * 10 - 9;
+        }
+        echo "<div class='w3-container w3-row'>";
+        echo "<center>";
+        for ($page = 1; $page <= $number_of_page; $page++) {
+            echo '<a href = "index.php?pageno=' . $page . '" style=
+            "text-decoration: none">&nbsp&nbsp' . $page . ' </a>';
+        }
+        echo " ( " . $pageno . " )";
+        echo "</center>";
+        echo "</div>";
+        ?>
+        <br>
+    </div>
+
+    <footer class="w3-footer w3-center" style="background-color: #133764 ; color:white">
         <p>Find me on social media.</p>
 
         <a href="https://www.facebook.com/qingyunleong" target="_blank"><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
